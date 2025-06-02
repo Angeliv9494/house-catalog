cb2c32449ee55c971ed30ec156395a5a6e599f2d
Angeliv <angivan94@gmail.com>
2025-06-02 13:34:54 -0600

    Added sold indicator

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -12,6 +12,7 @@ export const ProductGrid = (props: ProductGridProps) => {
     return (
       <div className="product-card" key={product.id}>
         <img src={product.image} alt={product.name} className="product-image" />
+        {product.quantity === 0 && (<img src="./vendido.png" alt="vendido" className="sold-image"/>)}
         <h1 className="product-name">{product.name}</h1>
         <h2 className="product-description">{product.description}</h2>
         <p className="product-price">{product.price.toLocaleString('en-US',{style: 'currency' , currency : 'MXN'})}</p>
