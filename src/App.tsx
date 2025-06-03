3dea16a9de05d8d2e1d8e31b18425b64f3360557
Angeliv <angivan94@gmail.com>
2025-06-02 23:34:53 -0600

    Simple Slider added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import productCatalog from "./data/products";
 import { Header } from "./components/Header.tsx";
 import { Footer } from "./components/Footer.tsx";
 import { ProductGrid } from "./components/ProductGrid.tsx";
+import { Preview } from "./components/Preview.tsx";
 
 function App() {
+  const test = true;
+
   return (
     <>
       <Header />
 
       <ProductGrid products={productCatalog} />
+
+      {test && <Preview />}
 
       <Footer />
     </>
--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,16 +1,36 @@
 import "./Preview.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Slider from "react-slick";
 
 export const Preview = () => {
   const modalClasses = "modal show hide";
 
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
   return (
     <>
       <div className="filter-background"></div>
 
       <div className={modalClasses}>
-        <h1>Product Name</h1>
-        <h2>Price</h2>
+        <div className="slider-wrapper">
+          <Slider {...settings}>
+            <div className="carousel-item">
+              <p className="carousel-image">1</p>
+            </div>
+
+            <div className="carousel-item">
+              <p className="carousel-image">2</p>
+            </div>
+          </Slider>
+        </div>
       </div>
     </>
   );
