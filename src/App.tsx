136512a0ad752fcdab245e191ef31469be6d7d43
Angeliv <angivan94@gmail.com>
2025-06-03 11:18:05 -0600

    Rolled back image previews

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,13 @@ import productCatalog from "./data/products";
 import { Header } from "./components/Header.tsx";
 import { Footer } from "./components/Footer.tsx";
 import { ProductGrid } from "./components/ProductGrid.tsx";
-import { Preview } from "./components/Preview.tsx";
 
 function App() {
-  const testModal = true;
-
   return (
     <>
       <Header />
 
-      {!testModal && <ProductGrid products={productCatalog} />}
-
-      {testModal && <Preview productId={1}/>}
+      <ProductGrid products={productCatalog} />
 
       <Footer />
     </>
