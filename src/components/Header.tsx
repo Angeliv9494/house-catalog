8a24506163e3f066776833fd30fbc61765f15973
Angeliv <angivan94@gmail.com>
2025-06-03 19:49:37 -0600

    Fixed Typos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import "./Header.css"
 export const Header = () => {
   return (
     <header className="header">
-      <img src="/box.png" alt="Logo" className="header_logo" />
+      <img src="/house-catalog/box.png" alt="Logo" className="header_logo" />
       <h1 className="header_title">Catálogo de Productos</h1>
     </header>
   );
